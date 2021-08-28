15876a994fbc63999fd0e1e5c486044cd2706150
jell <jl1096@ajou.ac.kr>
2021-08-28 23:50:11 +0900

    update

--- Study/programmers/2018KAKAO/17678.py
+++ Study/programmers/2018KAKAO/17678.py
@@ -1,1 +1,55 @@
 #[1차] 셔틀버스
+
+from collections import deque
+def solution(n, t, m, timetable):
+    timetable = sorted(timetable,reverse=True)
+    # 셔틀버스 타임테이블 만들기
+    bus_timetable = {}
+    s_time = '09:00'
+    bus_timetable[s_time] = deque(maxlen=m)
+    for i in range(1,n):
+        s_time_m = int(s_time[3:])
+        s_time_h = int(s_time[0:2])
+        s_time_m += t
+        if s_time_m>59:
+            s_time_m -= 60
+            s_time_h += 1
+        s_time = str(s_time_h).zfill(2)+':'+str(s_time_m).zfill(2)
+        bus_timetable[s_time] = deque(maxlen=m)
+
+    #탑승 시키기
+    for key in bus_timetable:    
+        for time in timetable:
+            if time <= key:
+                bus_timetable[key].append(time)
+                continue
+                
+    for key in reversed(bus_timetable):
+        if len(bus_timetable[key]) == m:
+            if len(set(bus_timetable[key])) == 1:
+                t = bus_timetable[key][0]
+                hour = int(t[0:2])
+                min = int(t[3:])-1
+                if min < 0:
+                    answer = str(hour-1).zfill(2)+':59'
+                else:
+                    answer = str(hour).zfill(2)+':'+str(min).zfill(2)
+
+            else:
+                answer = sorted(bus_timetable[key])[0]
+        else:
+            answer = key
+        break
+    
+    print()
+    print(timetable)
+    print(bus_timetable)
+    print(answer)
+    return answer
+
+solution(1,1,5,	["08:00", "08:01", "08:02", "08:03"])
+solution(2,10,2,["09:10", "09:09", "08:00"])
+solution(2,1,2,["09:00", "09:00", "09:00", "09:00"])
+solution(1,1,5,	["00:01", "00:01", "00:01", "00:01", "00:01"])
+solution(1,1,1,["23:59"])
+solution(10,60,45,["23:59","23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"])
--- Study/programmers/2018KAKAO/17679.py
+++ Study/programmers/2018KAKAO/17679.py
@@ -1,12 +1,33 @@
 # [1차] 프렌즈 4블록
 
-#지워지는 블록의 개수
+#solution 참고
 
-def pop(m, n ,board):
-    
-    return 
+#지워지는 블록의 개수
+def popping(m, n ,board):
+    pop_set = set()
+    for i in range(1,n):
+        for j in range(1,m):
+            if board[i][j] == board[i-1][j] == board[i][j-1] == board[i-1][j-1] != ' ':
+                pop_set |= set([(i, j), (i-1, j-1), (i-1, j), (i, j-1)])
+
+    for i, j in pop_set:
+        board[i][j] = 0
+
+    for i, row in enumerate(board):
+        empty = [' ']*row.count(0)
+        board[i] = empty + [block for block in row if block!=0]
+
+    return len(pop_set)
 
 def solution(m, n, board):
-    
     answer = 0
-    return answer
+    b = list(map(list, zip(*board)))
+    while True:
+        pop = popping(m, n, b)
+        if pop == 0:
+            return answer
+        answer += pop
+
+print(solution(4,5,["CCBDE", "AAADE", "AAABF", "CCBBF"]))
+print(solution(6,6,["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]))
+        
